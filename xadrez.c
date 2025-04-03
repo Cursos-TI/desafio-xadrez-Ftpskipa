c49e6e88b965619f4457df7de270d94db7728e1a
Ftpskipa <for.new.then@gmail.com>
2025-04-03 17:47:19 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,8 @@ int main()
 	const int movTorre = 5; // 5 casas para direita
 	const int movBispo = 5; // 5 casas para cima direita
 	const int movRainha = 8; // 8 casas para esquerda
+	const int movCavaloBai = 2; // 2 casas para baixo
+	const int movCavaloEsq = 1; // 1 casa para esquerda
 
 	printf("\nMovimentando a Torre:\n");
 	for (int i = 0; i < movTorre; i++)
@@ -25,6 +27,24 @@ int main()
 		moveuRainha++;
 	} while(moveuRainha < movRainha);
 	
+	int movCavaloCompleto = 1;
+	int moveuCavaloBai = 0;
+	int moveuCavaloEsq = 0;
+	printf("\nMovendo o Cavalo:\n");
+	for (int i = 0; i < movCavaloCompleto; i++)
+	{
+		while(moveuCavaloBai < movCavaloBai)
+		{
+			printf("Baixo\n");
+			moveuCavaloBai++;
+		}
+
+		do {
+			printf("Esquerda\n");
+			moveuCavaloEsq++;
+		} while(moveuCavaloEsq < movCavaloEsq);
+	}
+
 	printf("\n");
 	return 0;
 }
